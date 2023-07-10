28a7e59043925c80102d72da3d14f9112bec667e
ajrhind <alanah.rhind009@gmail.com>
2023-07-11 11:27:30 +1200

    class one

--- maeda_clock.js
+++ maeda_clock.js
@@ -1,9 +1,45 @@
+// // Update this function to draw you own maeda clock on a 960x500 canvas
+// function draw_clock(obj) {
+//   // YOUR MAIN CLOCK CODE GOES HERE
+//   background(50); //  beige
+//   fill(200); // dark grey
+//   textSize(40);
+//   textAlign(CENTER, CENTER);
+//   text("YOUR MAEDA CLOCK CODE GOES HERE", width/2, height/2);
+// }
+
 // Update this function to draw you own maeda clock on a 960x500 canvas
+let midX = 480;
+let midY = 250;
+
+
+// YOUR MAIN CLOCK CODE GOES HERE
 function draw_clock(obj) {
-  // YOUR MAIN CLOCK CODE GOES HERE
-  background(50); //  beige
-  fill(200); // dark grey
-  textSize(40);
-  textAlign(CENTER, CENTER);
-  text("YOUR MAEDA CLOCK CODE GOES HERE", width/2, height/2);
+//--------------------------------------------------
+  angleMode(DEGREES);
+  background(30); //  beige
+  textAlign(CENTER,CENTER);
+//----------------------------------------------------
+let size = 100
+let hours = obj.hours
+let minutes = obj.minutes
+let seconds = obj.seconds
+var sMap = map(obj.seconds, 0, 59, 50 ,450);
+var mMap = map(obj.minutes ,0,59,50,450);
+var hMap = map(obj.hours,0,23,50,450);
+var hColour = map(obj.hours,0,23,100,255);
+var mColour = map(obj.minutes,0,59,100,255);
+var sColour = map(obj.seconds,0,59,100,255);
+
+textSize(30);
+
+// for(i=0;i<23;i++){
+fill(100,100,hColour);
+  text(hours,midX-200,hMap);//hours
+// }for(i=0;i<59;i++){
+fill(100,mColour,100);
+  text(minutes,midX,mMap);
+fill(sColour,100,100);
+  text(seconds,midX+200,sMap);
+
 }
